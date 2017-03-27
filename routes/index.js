07ad93639473eb5d739378597b110f604bd515c4
mkozhevnikov <mak.egik@gmail.com>
2017-03-28 02:17:47 +0500

    : utorrent-api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var Client = require('utorrent-api');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  var utorrent = new Client('localhost', '8080');
+  utorrent.setCredentials('admin', '123qweasd');
+  
+  utorrent.call('list', function(err, torrents_list) {
+      if(err) { console.log(err); return; }
+  
+      console.log(torrents_list);
+  });
 });
 
 module.exports = router;
